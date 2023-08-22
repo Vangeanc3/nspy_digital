e5e1f8f4b66ecaf9e8905023bdcb06f9d43d4cc4
Ismael <ismaelmartins919@gmail.com>
2023-08-22 01:31:42 -0300

    add pre release

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'NSPY Digital',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.myTheme,
       initialRoute: "/chat",
